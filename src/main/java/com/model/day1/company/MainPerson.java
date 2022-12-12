fc47735110d6abfb7a749959b9bf0081ad40e3e7
Paulina Pionk <paulinapionk@gmail.com>
2022-12-12 22:01:49 +0100

    Streams excercises

---
+++ src/main/java/com/model/day1/company/MainPerson.java
@@ -0,0 +1,14 @@
+package com.model.day1.company;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MainPerson {
+    private String name;
+    private int age;
+}
